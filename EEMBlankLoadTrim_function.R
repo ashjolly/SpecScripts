a37ab0c4cc9d35f986714f520f9f565d5e9d23ca
ashjolly <ashlee.jollymore@gmail.com>
2015-06-29 12:52:24 -0700

    Initial comitt for script with function for loading and trimming blank files from Aqualog for correction and modelling.

---
+++ EEMBlankLoadTrim_function.R
@@ -0,0 +1,37 @@
+#
+#
+#
+# FUnction to load and trim blank files from Aqualog
+# 29june2015
+########
+
+BLANKtrim <- function() {
+  #read in blank file
+  # Blankfilename <- test2[i,2] # set blank file for the sample
+  Blankfilename <- toString(data.3[i,4]) 
+  #setwd(directoryblank) 
+  Blankfile <- read.delim(Blankfilename, header= FALSE, sep = "")
+  
+  #### Blank file trimming
+  # take out the first two rows and first two columns of data in Blank - these contain text
+  
+  #emission = y axis. get as row names
+  y = nrow(Blankfile)
+  em = as.numeric(t(data.frame(Blankfile[c(3:y), 1])))
+  em_all[i,] = em
+  
+  #excitation - x axis. Get as column names.
+  x = ncol(Blankfile) 
+  ex_initial = as.numeric(Blankfile[1, c(4:x)])
+  ex = as.numeric((sort(Blankfile[1, c(4:x)], decreasing = FALSE)))
+  ex_all[i,] = ex
+  
+  # cut Blank files
+  Blkcor <- Blankfile[c(2:y), c(2:x)]  
+  
+  #Assign col names as ex and row names as emission
+  colnames(Blkcor) <- ex_initial
+  rownames(Blkcor) <- em
+  
+  return(Blkcor)
+}
