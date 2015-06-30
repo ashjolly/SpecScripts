dea7dbd02dbbd63b179f28408c16e16558013f1d
ashjolly <ashlee.jollymore@gmail.com>
2015-06-29 18:31:48 -0700

    Replaced i with loopnum variable

--- EEMfileLoadTrim_function.R
+++ EEMfileLoadTrim_function.R
@@ -5,18 +5,18 @@
 # 29june2015
 ########
 
-EEMtrim <- function(graphheadings, samplewd) {
+EEMtrim <- function(graphheadings, samplewd, loopnum) {
   
   #set wd where all of the EEM files are located. Note in .dat format
   setwd(samplewd)
   
   # Read in the EEM file
-  EEMSfilename <- toString(graphheadings[i,2]) # where the EEMS file are located in the graph headings file 
+  EEMSfilename <- toString(graphheadings[loopnum,2]) # where the EEMS file are located in the graph headings file 
   
   # read in EEM file
   EEMSfile <- read.delim(EEMSfilename, header= FALSE, sep = "")
   #samplename <- strapplyc(EEMSfilename, "001(.*)PEM", simplify = TRUE)
-  samplename <- toString(graphheadings[i,1]) #column with the sample IDs
+  samplename <- toString(graphheadings[loopnum,1]) #column with the sample IDs
 
   ############## trim and arrange the eem files
   # EEM file
@@ -24,12 +24,16 @@ EEMtrim <- function(graphheadings, samplewd) {
   x = ncol(EEMSfile)
   ex_initial = as.numeric(EEMSfile[1, c(4:x)])
   ex = as.numeric((sort(EEMSfile[1, c(4:x)], decreasing = FALSE)))
-  ex_all[i,] = ex
+  
+  #ex_all is a variable that holds all of the ex wavelengths. It's a way of seeing if ex wavelengths are different between samples
+  ex_all[loopnum,] = ex
 
   #emission = y axis
   y = nrow(EEMSfile)
   em = as.numeric(t(data.frame(EEMSfile[c(3:y), 1])))
-  em_all[i,] = em
+  
+  #em_all is a variable that holds all of the em wavelengths. It's a way of seeing if em wavelengths are different between samples
+  em_all[loopnum,] = em
 
   # take out the first three rows and first column of data in EEMS. These just contain text
   EEMScut <- EEMSfile[c(3:y), c(2:x)]
