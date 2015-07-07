fa2a88255465567ca03e60e3d77501647aac6a27
ashjolly <ashlee.jollymore@gmail.com>
2015-07-07 16:12:28 -0700

    last revisions to IFE file

--- EEMIFECorr_function.R
+++ EEMIFECorr_function.R
@@ -8,28 +8,56 @@
 # Note that this gives the IFE correction factor, with the actual correction calculated in the loop
 ############
 
+# EEM.IFC = EEM*10.^(0.5*IFC) #perform inner filter calculation
 
-innerfilter <- function(eem, abs, em) {
+innerfilter <- function(eem, abs, emfluor, exfluor) {
   
-  waves = colnames(abs)  # wavelengths at which abs collected
+  # em fluor = emission wavelength at which fluorescence EEM was collected
+  # ex fluor = excitation wavelength at which fluorescence EEM was collected
+  
+  # Need to interpolate abs wavelength to reflect wavelength at which fluorescence was collected
+  
+  waves = as.numeric(gsub("X", "", colnames(abs)))  # wavelengths at which abs collected
+ 
   data = abs # column containing absorbance data
   
-  ex_abs = data # wavelengths at which the absorbance emission was collected
+  # Interpolation absorbance for emission wavelengths
+  emforIFC = em # emission wavalengths from fluorescence
   
-  # em data
-  emforIFC = em # note that CM only takes from 1:125 of the emission wavelenghts.. apply to all?
-  em_abs = approxfun(x = waves, y = data, xout = emforIFC) #em_abs = interp1(waves,data,emforIFC)'; - initial CM line
+  # note that CM only takes from 1:125 of the emission wavelenghts.. apply to all?
+  em_abs = as.data.frame(approx(x = waves, y = data, xout = emforIFC, method = "linear")) #em_abs = interp1(waves,data,emforIFC)'; - initial CM line
 
   #note that CM file also inserts 0.175 as first em_abs number? Code below
   #em_abs(2:125) = em_abs(1:124);
   #em_abs(1) = 0.1705;
-
+  
+  # Interpolation absorbance for excitation wavelengths
+  exforIFC = ex
+  ex_abs = as.data.frame(approx(x = waves, y = data, xout = exforIFC, method = "linear"))
+  IFC <- matrix(ncol = (dim(ex_abs)[1]), nrow = (dim(em_abs)[1]))
+  colnames(IFC) <- ex_abs[,1]
+  rownames(IFC) <- em_abs[,1]
+  
   #loop adapted from CM file
-  for (f in 1:length(em_abs)) {
-     for (b in 1:length(ex_abs)) {
-         IFC(f,b)=ex_abs(f)+em_abs(b)
+  for (f in 1:(dim(em_abs)[1])) {
+     for (b in 1:(dim(ex_abs)[1])) {
+         IFC[f,b]=ex_abs[f,2]+em_abs[b,2]
      }
   }  
   
-  return(IFC)
-}
+  # Apply IFE (ODex + ODem) to EEMs
+  # EEM.IFE = EEM *10 ^(0.5 *IFC) Need to have something to ensure that if IFC = Na, 
+  
+  IFC <- as.data.frame(IFC)
+  IFC.2 <- IFC *0.5
+  IFC.2 <- as.data.frame(10^IFC.2)
+  
+  # replace Nas in IFC with 1
+  IFC.2[is.na(IFC.2)] <- 1
+  
+  # get corrected fluorescence
+  EEM.IFC <- eem * IFC.2
+  
+  return(EEM.IFC)
+  
+  # will need to do something fro Nas arising because of differences in wavelengths
