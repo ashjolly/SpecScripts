7eb6b48a64d68da3939252b8956f8b6b8d6affad
ashjolly <ashlee.jollymore@gmail.com>
2015-07-08 09:47:25 -0700

    small change to reflect CM

--- EEMRaleigh_function.R
+++ EEMRaleigh_function.R
@@ -17,17 +17,17 @@ raleigh <- function(eem, slitwidth){
   em = as.numeric(rownames(Acut))
   
   for (f in 1:length(ex)) {
-    tempem = (em>(ex[f]+slitwidth))
+    tempem = (em<(ex[f]+slitwidth))
     Acut[tempem,f]= 0
   }
   
   #second order
-  for (j in 1:length(ex)) {
+  for (f in 1:length(ex)) {
   #temp = find(em>(ex(f)*2-slitwidth));
-    temp = (em>(ex[j]*2-slitwidth))
+    temp = (em>(ex[f]*2-slitwidth))
     Acut[temp,j]= 0
   }
 
   return(Acut)  
-  
+
 }
