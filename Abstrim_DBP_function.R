977b2a0c60ff711f5e0d76a44d57b79f4152ca9e
ashjolly <ashlee.jollymore@gmail.com>
2015-07-10 19:43:38 -0700

    abs trim function. for trimming abs file

---
+++ Abstrim_DBP_function.R
@@ -0,0 +1,30 @@
+# 
+# abs trim function
+
+# ashlee jollymore's phd project
+# 10july2015
+#############
+
+abstrim <- function(abs, minex) {
+  
+  #trim so that exitation and emission goes from the same
+  ex.temp <- rownames(abs)
+  
+  x = dim(abs)[1]
+  
+  # trim for min excitation - useful if you have wavelengths that start at different points
+  if(ex.temp[x] != minex) {
+    
+    # if last value in ex.temp is not the min ex you specift, trim to this
+    
+    # find column where the exitation wavelength is new min ex to cut from
+    xmin = abs[minex,]
+    trim.abs <- as.data.frame(abs[c(1:minex),])
+  } 
+  
+  if (ex.temp[x] == minex){
+    trim.abs <- abs
+  }
+  
+  return(trim.abs)
+}
