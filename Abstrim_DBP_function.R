d7e4beb98cf472a4548bd3aa657d71c32e42e4b0
ashjolly <ashlee.jollymore@gmail.com>
2015-07-27 15:42:37 -0700

    small changes for calc abs

--- Abstrim_DBP_function.R
+++ Abstrim_DBP_function.R
@@ -8,17 +8,17 @@
 abstrim <- function(abs, minex) {
   
   #trim so that exitation and emission goes from the same
-  ex.temp <- as.numeric(rownames(abs))
+  ex.temp <- colnames(abs)
   
-  x = dim(abs)[1]
+  x = dim(abs)[2]
   
   # trim for min excitation - useful if you have wavelengths that start at different points
   if(ex.temp[x] != minex) {
     # if last value in ex.temp is not the min ex you specift, trim to this
     
     # find column where the exitation wavelength is new min ex to cut from
-    xmin = as.numeric(match(minex,rownames(abs)))
-    trim.abs <- as.data.frame(abs[c(1:xmin),])
+    xmin = match(minex,colnames(abs))
+    trim.abs <- as.data.frame(abs[c(1:xmin)])
   } 
   
   if (ex.temp[x] == minex){
