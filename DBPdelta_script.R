d90a98af20a270c0f98bf799e31761fb17207baf
ashjolly <ashlee.jollymore@gmail.com>
2015-07-27 17:37:10 -0700

    Tried to insert function for caulcting abs indicies from Abspre-Abs.post, but because a lot of the numbers are negative, calculating the spectral slopes doesn't work. Taken out.

--- DBPdelta_script.R
+++ DBPdelta_script.R
@@ -1,6 +1,7 @@
 #
 # Script for creating delta DBP EEMS
 # The aim of this script is to get the eems of the prechlorination minus the post chlorination
+# Also, to do the same for the absorbance file and calculate the absorbance indicies from this
 # DBP project, ashlee's phd
 # 9July2015
 # ashlee jollymore
@@ -26,54 +27,88 @@ postchlor.files <- "/Users/ashlee/Documents/UBC Data/DBP_data/DBP_fluorescence/D
 delta.eems <- "/Users/ashlee/Documents/UBC Data/DBP_data/DBP_fluorescence/DBP_delta"
 
 project <- "DBPdelta"
-# create file that matches the pre and post files together
 
-# prechlor files
+##################
+# create file that matches the pre and post files together
+######## Prechlor files
 setwd(prechlor.files)
-filelist_pre <- list.files(pattern = "CorrectedNEW.csv$")
-# Get sample ID from pre chlor files
+filelist_pre <- list.files(pattern = "_Corrected.csv$")
+filelist_preAbs <- list.files(pattern = "AbsCorrected.csv$")
+
+# Get sample ID from pre chlor files - EEMS
 sample.ID <- 0 #create sample ID variable
 n = length(filelist_pre)
 for (i in 1:n){
-  sampleID.pre.temp <- strapplyc(filelist_pre[i], "DBP(.*)_DBPPre_CorrectedNEW", simplify = TRUE)
+  sampleID.pre.temp <- strapplyc(filelist_pre[i], "DBP(.*)_DBPPre_Corrected.csv", simplify = TRUE)
   sample.ID[i] <- sampleID.pre.temp 
 }
 
-filelist_pre <- as.data.frame(cbind(filelist_pre, sample.ID))
+filelist_preEEMS <- as.data.frame(cbind(filelist_pre, sample.ID))
 
+###
+# Get sample ID from pre chlor files - Abs
+sample.ID <- 0 #create sample ID variable
+n = length(filelist_preAbs)
+for (i in 1:n){
+  sampleID.pre.temp <- strapplyc(filelist_preAbs[i], "DBP(.*)_DBPPre_AbsCorrected.csv", simplify = TRUE)
+  sample.ID[i] <- sampleID.pre.temp 
+}
+
+filelist_preAbs <- as.data.frame(cbind(filelist_preAbs, sample.ID))
+
+filelist_pre <- as.data.frame(merge(filelist_preEEMS, filelist_preAbs, by = "sample.ID"))
+
+######################
 # Get sample ID from post chlor files
 #postchlor
 setwd(postchlor.files)
-filelist_post <- list.files(pattern = "CorrectedNEW.csv$")
+filelist_post <- list.files(pattern = "_Corrected.csv$")
+filelist_postAbs <- list.files(pattern = "AbsCorrected.csv$")
 
+# get sample ID from EEMS
+sample.ID <- 0 #reset sample ID variable
 n = length(filelist_post)
 for (i in 1:n){
-  sampleID.post.temp <- strapplyc(filelist_post[i], "DBPChlor(.*)_DBPPost_CorrectedNEW", simplify = TRUE)
+  sampleID.post.temp <- strapplyc(filelist_post[i], "DBPChlor(.*)_DBPPost_Corrected.csv", simplify = TRUE)
   sample.ID[i] <- sampleID.post.temp 
 }
 
-filelist_post <- as.data.frame(cbind(filelist_post, sample.ID))
+filelist_postEEMS <- as.data.frame(cbind(filelist_post, sample.ID))
+
+# get sample ID for post Abs
+sample.ID <- 0 #reset sample ID variable
+
+n = length(filelist_postAbs)
+for (i in 1:n){
+  sampleID.post.temp <- strapplyc(filelist_postAbs[i], "DBPChlor(.*)_DBPPost_AbsCorrected.csv", simplify = TRUE)
+  sample.ID[i] <- sampleID.post.temp 
+}
+
+filelist_postAbs <- as.data.frame(cbind(filelist_postAbs, sample.ID))
+filelist_post <- as.data.frame(merge(filelist_postEEMS, filelist_postAbs, by = "sample.ID"))
 
 # merge files by sample ID
 all <- merge(filelist_post, filelist_pre, by = "sample.ID", all = FALSE)
-Fluor.ind = data.frame(matrix(vector(), 0, 8))
 
-# load each file and do the subtraction
+Spec.ind = data.frame(matrix(vector(), 0, 8))
+
+#### Run a loop  that loads each file and do the subtraction
 
 n = dim(all)[1]
 for (i in 1:n){
-  # pre files
+  
+  ##### Subtract EEM files
+  # pre EEM files
   setwd(prechlor.files)
-  pre.samplename <- toString(all[i,3])
+  pre.samplename <- toString(all[i,4])
   pre.temp <- as.data.frame(read.delim(pre.samplename, header= TRUE, sep = ",", stringsAsFactors=FALSE))
   
-  #post files
+  #post EEM files
   setwd(postchlor.files)
   post.samplename <- toString(all[i,2])
   post.temp <- as.data.frame(read.delim(post.samplename, header= TRUE, sep = ",", stringsAsFactors=FALSE))
   
   # ensure that EEMS are the same size
-  
   #trim so that exitation and emission goes from the same
   # call EEM trim function
   setwd("/Users/ashlee/SpecScripts") 
@@ -86,13 +121,70 @@ for (i in 1:n){
   delta.temp <- pre.trim - post.trim
   
   # get generalized sample ID - for example, DBP0001
-  sample.ID <- strapplyc(pre.samplename, "(.*)_DBPPre_CorrectedNEW", simplify = TRUE)
+  sample.ID <- strapplyc(pre.samplename, "(.*)_DBPPre_Corrected", simplify = TRUE)
   
   # save delta eem file
   deltapath <- file.path(delta.eems, paste(sample.ID,"_", project,".csv", sep = ""))
   write.table(delta.temp, file = deltapath, row.names = TRUE,col.names = TRUE, sep = ",")
 
   ##########
+  # Plot delta eems as a contour plot
+  # variables to change
+  zmax = max(delta.temp,na.rm=TRUE) # put the max intensity of that you want to graph
+  #EEMmax[i] <- zmax #to show the maximum fluorescence for all files
+  xlimit <- range(300, 700, finite=TRUE)
+  ylimit <- range(240, 450, finite = TRUE)
+  
+  numcont = 100 # number of contour levels you want: Change if you want
+  
+  ##### contour plotting function
+  library(gplots)
+  
+  # call contour plot function
+  setwd("/Users/ashlee/SpecScripts") 
+  source("EEM_contour_v1.R")
+  
+  #Plot contours and save in correction file
+  plotpath <- file.path(delta.eems, paste(sample.ID,"_", project,".jpeg", sep = ""))
+  
+  g <- length(delta.temp)
+  EEMplot <- data.frame(delta.temp) # not cutting out the last two columns
+
+  #EEMplot[EEMplot<0]=0 # have line so that if fluorescence is less than 0, becomes 0.
+  explot = seq(240, 800, by = 2) 
+  emplot = as.numeric(row.names(EEMplot))
+  
+  jpeg(file=plotpath)
+  contour.plots(eems = as.matrix(EEMplot), Title = paste(sample.ID, "Delta", sep=""), ex = explot, em = emplot)  
+  dev.off()
+  
+  ########## Calculating spectral indicies
+  # Abs indicies
+  # Calculate delta abs indicies
+  # pre EEM files
+  setwd(prechlor.files)
+  preabs.samplename <- toString(all[i,5])
+  preabs.temp <- as.data.frame(read.delim(preabs.samplename, header= TRUE, sep = ",", stringsAsFactors=FALSE))
+  
+  #post EEM files
+  setwd(postchlor.files)
+  postabs.samplename <- toString(all[i,3])
+  postabs.temp <- as.data.frame(read.delim(postabs.samplename, header= TRUE, sep = ",", stringsAsFactors=FALSE))
+  
+  ############# trim
+  # Ensure that absorbance files go from 800-240 nm
+  #trim so that exitation and emission goes from the same
+  # call function
+  setwd("/Users/ashlee/SpecScripts") 
+  source("Abstrim_DBP_function.R")
+  
+  pre.trim <- abstrim(abs = preabs.temp, minex = "X240")
+  post.trim <- abstrim(abs = postabs.temp, minex = "X240")
+  
+  ############# calculate pre - post
+  delta.abs <- pre.trim - post.trim
+  
+   ##########
   # Calculating fluorescence indicies
   # call function
   setwd("/Users/ashlee/SpecScripts") 
@@ -124,46 +216,18 @@ for (i in 1:n){
   
   # call function that calculates fluorescent indicies
   Fluor.indtemp <- Fluor(eem = delta.temp)
+  
   # bind sample ID
   Fluor.indtemp <- cbind(sample.ID, Fluor.indtemp)
-  Fluor.ind[i,] <- Fluor.indtemp
-  colnames(Fluor.ind) <- colnames(Fluor.indtemp)
-  
-  ##########
-  # last - plot delta eems as a contour plot
-  #variables to change
-  zmax = max(delta.temp,na.rm=TRUE) # put the max intensity of that you want to graph
-  #EEMmax[i] <- zmax #to show the maximum fluorescence for all files
-  xlimit <- range(300, 700, finite=TRUE)
-  ylimit <- range(240, 450, finite = TRUE)
-  
-  numcont = 100 # number of contour levels you want: Change if you want
-  
-  ##### contour plotting function
-  library(gplots)
-  
-  # call contour plot function
-  setwd("/Users/ashlee/SpecScripts") 
-  source("EEM_contour_v1.R")
-  
-  #Plot contours and save in correction file
-  plotpath <- file.path(delta.eems, paste(sample.ID,"_", project,".jpeg", sep = ""))
-  
-  g <- length(delta.temp)
-  EEMplot <- data.frame(delta.temp) # not cutting out the last two columns
-
-  #EEMplot[EEMplot<0]=0 # have line so that if fluorescence is less than 0, becomes 0.
-  explot = seq(240, 800, by = 2) 
-  emplot = as.numeric(row.names(EEMplot))
-  
-  jpeg(file=plotpath)
-  contour.plots(eems = as.matrix(EEMplot), Title = paste(sample.ID, "Delta", sep=""), ex = explot, em = emplot)  
-  dev.off()
+  Spec.ind[i,] <- Fluor.indtemp
+  colnames(Spec.ind) <- colnames(Fluor.indtemp)
 }
 
 #### End of loop!
 #write file containing spectral indicies + sample IDs
 #after loop is finished with all samples
 corrpath <- file.path(delta.eems, paste(project, "FluorIndicies.csv", sep = ""))
-write.table(Fluor.ind, file = corrpath, row.names = FALSE, col.names = TRUE, sep = ",")
+write.table(Spec.ind, file = corrpath, row.names = FALSE, col.names = TRUE, sep = ",")
+
+
 
