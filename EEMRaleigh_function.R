0e4f2021b8b1343633892ffbb875255b24a9fa9f
ashjolly <ashlee.jollymore@gmail.com>
2015-08-05 17:07:02 -0700

    trying interpolation function for second order raleigh

--- EEMRaleigh_function.R
+++ EEMRaleigh_function.R
@@ -34,6 +34,40 @@ raleigh <- function(eem, slitwidth){
     tempem = as.character(subset(em, em >=lower & em <= upper))
     Acut[c(tempem),j]= 0
   }
+  
+  # identify Raman lines and replace with zeros
+  # this is a single line with ex = 350, emission from 
+  # Note that the majority of this should be taken care of by subtracting the blank, but this region can be excised anyways
+  
+  
+  ################# Interpolation
+  # as per the eemscat.m script by Rasmus Bro et al.
+  # interpolation code from this script:
+  # mm=interp1(ax2cut,Eendcut,ax2,'pchip','extrap'); %%% interpolation using cubic option
+
+  # Interpolate second order Raleigh lines
+  library('pracma')
+  for (j in 1:length(ex)){
+    
+    upper = (ex[j]*2)+slitwidth
+    lower = (ex[j]*2)-slitwidth
+    tempem = as.character(subset(em, em >=lower & em <= upper))
+    
+    # Cut out the zeros, replace with NaN
+    Acut[c(tempem),j]= 0
+    temp.data = cbind(em, Acut[,j])
+    rownames(temp.data) = em
+    
+    #interpolate 
+    #interp1(x, y, xi = x,
+    #        method = c("constant", "linear", "nearest", "spline", "cubic"))
+    test = interp1(temp.data[,1], temp.data[,2], temp.data[(tempem),2], method = c("cubic"), extrap = 'extrap')
+    
+    Acut[tempem,j]= interp1(em, Acut[,j], as.numeric(tempem), 
+                            method = c("cubic"), 'extrap')
+    
+    
+  }
 
   return(Acut)  
 
