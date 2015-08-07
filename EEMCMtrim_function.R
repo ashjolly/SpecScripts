a054d7b8562b269925cf94693f1a5ee8152f5003
ashjolly <ashlee.jollymore@gmail.com>
2015-08-07 15:41:58 -0700

    Script for saving EEMS in COry McKNight folders, including trimmed and properly named EEMS, ex and em files, andf graph heading files

--- EEMCMtrim_function.R
+++ EEMCMtrim_function.R
@@ -6,47 +6,49 @@
 # 8July2015
 ############
 
-CMtrim <- function(directory, projectname, minex){
+CMtrim <- function(filedirectory, filelist, project, exmin, directoryCM, ex){
   
-  # get directory with all of the corrected files
-  setwd(directory) 
-  filelist_EEMScor <- list.files(pattern = "_Corrected.csv$")
-
-  ######## Prepping files for Cory McKnight modelling in Matlab
-  ########
-  # Ensure that the files have the same ex and em lengths
-  #myfiles = lapply(filelist_EEMScor, read.delim)
-  #myfiles = do.call("rbind", lapply(filelist_EEMScor, 
-  #    function(x) read.delim(x,stringsAsFactors = FALSE)))
+  # create graph heading variable
+  graphheadings = data.frame((0))
   
-  # CM - take out row and column names in first column and row and save in CM folder
-  n = length(filelist_EEMScor)
+  # set working directory with filder that has corrected files
+  setwd(filedirectory)
   
-  # get the ex and em for each of the files
-  
+  n = length(filelist)
   for (i in 1:n){
-    temp.EEMS <- read.delim(filelist_EEMScor[i], header= TRUE, sep = ",")
-  
+    temp.EEMS <- read.delim(filelist[i], header= TRUE, sep = ",")
+    
     #trim so that exitation and emission goes from the same
     ex.temp <- colnames(temp.EEMS)
-  
-      if(ex.temp[1] != minex) {
+    
+    if(ex.temp[1] != exmin) {
       # if first value in ex.temp is not 240, trim 
       ex.length <- length(ex.temp)
       # find column where the exitation wavelength is 240 to cut from
-      xmin = as.numeric(match(minex,names(temp.EEMS)))
-      temp.EEMS <- temp.EEMS[,c(xmin:ex.length)]
-  } 
+      x240 = as.numeric(match(exmin,names(temp.EEMS)))
+      temp.EEMS <- temp.EEMS[,c(x240:ex.length)]
+    } 
+    
+    # Create graph headings variable. Also insert "_i" to use in CM modelling
+    samplename <- strapplyc(filelist_EEMScor[i], paste("(.*)_", project, "_Corrected", sep = ""), simplify = TRUE)
+    graphheadings[i,] <-paste(samplename, project,"CorrCM_",i, sep = "")
+    
+    # Resave trimmed EEM without the row and column names
+    corrpath <- file.path(directoryCM, paste(samplename, project,"CorrCM_",i,".csv", sep = ""))
+    write.table(temp.EEMS, file = corrpath, row.names = FALSE,col.names = FALSE, sep = ",")
+    
+  }
   
-  # cut out any columns containing Nas- this is 798 and 800 nm. Must cut last four rows of data from 20april2015
-  #temp.EEMS.1 <- na.omit(temp.EEMS)
-  #g <- length(temp.EEMS)
-  #temp.EEMS.1 <- temp.EEMS[,c(1:(g-4))] #cut out the last four colomns manually
+  # save ex and em in two separate files, to make it easier to read into CM PARAFAC files
+  corrpath <- file.path("/Users/ashlee/Documents/MATLAB/ExEmfiles", paste(project,"em",".csv", sep = ""))
+  write.table(em, file = corrpath, row.names = FALSE,col.names = FALSE, sep = ",")
   
-  #resave without the row and column names
-  samplename <- strapplyc(filelist_EEMScor[i], "(.*)Prechlor", simplify = TRUE)
-  corrpath <- file.path(directoryCM, paste(samplename,"CorrCM_",i,".csv", sep = ""))
-  write.table(temp.EEMS.1, file = corrpath, row.names = FALSE,col.names = FALSE, sep = ",")
+  corrpath <- file.path("/Users/ashlee/Documents/MATLAB/ExEmfiles", paste(project,"ex",".csv", sep = ""))
+  write.table(ex.PARAFAC, file = corrpath, row.names = FALSE,col.names = FALSE, sep = ",")
   
-}
+  # write graph headings file
+  corrpath <- file.path("/Users/ashlee/Documents/MATLAB/CM_graphheadings", paste("GraphHeadings_", project,".txt", sep = ""))
+  write.table(graphheadings, file = corrpath, row.names= FALSE, col.names = FALSE, sep= ",")
+  
+  return(graphheadings)
 }
