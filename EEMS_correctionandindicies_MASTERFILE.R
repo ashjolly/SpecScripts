74d22b894506f900ddfda839d9cc4a0cffcc86df
ashjolly <ashlee.jollymore@gmail.com>
2015-08-10 16:11:56 -0700

    Still trying to make so that only running functions - make corrections file as adaptable as possible!

--- EEMS_correctionandindicies_MASTERFILE.R
+++ EEMS_correctionandindicies_MASTERFILE.R
@@ -337,8 +337,6 @@ for (i in 1:n){
 
 ########################################################################################
 ######### Loop over corrected files in the to calculate indicies
-Spectral.Indicies = data.frame(matrix(vector(), 5000, 17)) #creating an empty vector
-
 # create master file with abs and EEMs corrected file names aligned according to sample ID
 # call function to create master file  with filenames
 setwd("/Users/ashlee/SpecScripts") 
@@ -349,81 +347,18 @@ filelist_EEMScor <- abseemfilecomp(directoryAbsEEMs = directoryCorrectedEEMS, pr
 # set directory with EEMS that you corrected according to the loop above
 # Call function that will loop over the files in this folder and apply abs and fluor correction indicies
 setwd("/Users/ashlee/SpecScripts") 
-source("AbsEEMSfilecomp_function.R")
+source("EEMSIndCalculation_function.R")
 
-spec.indicies = calc.indicies(fileist_EEMScor = filelist_EEMScor, directoryCorrectedEEMS = directoryCorrectedEEMS, waves = waves)
+spec.indicies = calc.indicies(filelist_EEMScor = filelist_EEMScor, directoryCorrectedEEMS = directoryCorrectedEEMS, waves = waves)
 
-n = dim(filelist_EEMScor)[1]
-
-for (i in 1:n){
-
-  ###########
-  # Calculating absorbance indicies
-  # load the Abs file
-  setwd(directoryCorrectedEEMS)
-  abs.temp <-as.data.frame(read.delim(as.character(filelist_EEMScor[i,3]), 
-                                      header= TRUE, sep = ",", stringsAsFactors=FALSE))
-  
-  # call function
-  setwd("/Users/ashlee/SpecScripts") 
-  source("Aqualog_Absindicies_v1.R")
-
-  #call the function to calculate indicies
-  Abs.ind <- Abs(absorbance = abs.temp)
-  #Abs.all[i] <- cbind(samplename, Abs.ind) #Put sample number
-
-  ##########
-  # Calculating fluorescence indicies
-  setwd(directoryCorrectedEEMS)
-  EEMcorr <-as.data.frame(read.delim(as.character(filelist_EEMScor[i,2]), 
-                                     header= TRUE, sep = ",", stringsAsFactors=FALSE))
-  
-  # wavelengths for Fluorescence indicies calculation
-  # ex wavelengths
-  ex370 <- as.numeric(grep(ex.370, colnames(EEMcorr)))
-  ex254 <- as.numeric(grep(ex.254, colnames(EEMcorr)))
-  ex310 <- as.numeric(grep(ex.310, colnames(EEMcorr)))
-  ex274 <- as.numeric(grep(ex.274, colnames(EEMcorr)))
-  ex276 <- as.numeric(grep(ex.276, colnames(EEMcorr)))
-  ex320 <- as.numeric(grep(ex.320, colnames(EEMcorr)))
-  ex340 <- as.numeric(grep(ex.340, colnames(EEMcorr)))
-  
-  # em wavelengths
-  em470 <- as.numeric(grep(em.470, rownames(EEMcorr)))
-  em520 <- as.numeric(grep(em.520, rownames(EEMcorr))) 
-  em435 <- as.numeric(grep(em.435, rownames(EEMcorr))) 
-  em480 <- as.numeric(grep(em.480, rownames(EEMcorr)))
-  em300 <- as.numeric(grep(em.300, rownames(EEMcorr)))
-  em345 <- as.numeric(grep(em.345, rownames(EEMcorr)))
-  em380 <- as.numeric(grep(em.380, rownames(EEMcorr)))
-  em420 <- as.numeric(grep(em.420, rownames(EEMcorr)))
-  em436 <- as.numeric(grep(em.436, rownames(EEMcorr)))
-  em350 <- as.numeric(grep(em.350, rownames(EEMcorr)))
-  em410 <- as.numeric(grep(em.410, rownames(EEMcorr)))
-  em430 <- as.numeric(grep(em.430, rownames(EEMcorr)))
-  
-  # call function
-  setwd("/Users/ashlee/SpecScripts") 
-  source("Aqualog_Fluorindicies_v2.R")
-
-  # call function that calculates fluorescent indicies
-  Fluor.ind <- Fluor(eem = EEMcorr)
-
-  ##########
-  # bind fluor indicies with abs indicies as well as the sample id
-  samplename <- as.character(filelist_EEMScor[i,1]) # column name where sample ID is 
-  
-  Spectral.Ind <- cbind(samplename, Abs.ind, Fluor.ind) 
-  top <- colnames(Spectral.Ind)
-  Spectral.Indicies[i,]  <- Spectral.Ind
-  colnames(Spectral.Indicies) <- top
-  }
+# Check spectral indicies
+spec.indicies
 
 ######## end of loop!
 #write file containing spectral indicies + sample IDs
 #after loop is finished with all samples
 corrpath <- file.path(directoryCorrectedEEMS, paste(project, "SpectralIndicies.csv", sep = ""))
-write.table(Spectral.Indicies, file = corrpath, row.names = FALSE, col.names = TRUE, sep = ",")
+write.table(spec.indicies, file = corrpath, row.names = FALSE, col.names = TRUE, sep = ",")
 
 ####################################################################################################################################
 ############################ Cutting EEMS for Cory McKnight and DOM Fluor toolbox
