e036094f6509dd0e53c014490d4545703ee738e8
ashjolly <ashlee.jollymore@gmail.com>
2015-08-19 14:49:23 -0700

    Normalization function works now!

--- WSA_analysis.R
+++ WSA_analysis.R
@@ -1,5 +1,3 @@
-#
-#
 # WSA Analysis script
 # For analyzing results from NVivo
 # 15Aug2015
@@ -22,26 +20,58 @@ library("d3heatmap")
 ## load data
 data <- as.data.frame(read.csv(paste(directory, 'test_heatmap.csv', sep = ''), sep = ",", header = TRUE))
 
+## Normalize data by the total number of responses within a group
+
 # group by policy group + count total number of responses
 total.responses <- aggregate(data[,3:18], by=list(Policy = data$Policy), FUN = sum)
 total.responses$status <- 'total'
-data1 <- rbind(data, total.responses)
 
-policy.areas <- unique(data1$Policy)
-# Normalize each entry by the total number of responses
-test <- subset(data1, data1$Policy == policy.areas[1])
-normalize <- test[c(1:3), c(3:18)]/test[4, c(3:18)]
+data$Policy_response <- paste(data1$Policy, data1$status, sep="_") # merge first two columns into third column
 
-# normalize by ddply
-#f <- function {
-#  # get total responses for each
-#  aggregate(data[,3:18], by = by=list(Policy = data$Policy))
+policy.areas <- unique(data$Policy)      # find the total number of unique policy areas
+stakeholders <- unique(colnames(data[,3:18]))
+
+#############################################
+# normalize by the total number of responses within a category
+# create a new dataframe with the normalized responses
+
+j = length(policy.areas)
+
+for(i in 1:j){
   
-#}
-#normalize <- apply(data[,3:18], 2, function)
+  polarea.temp <- policy.areas[i]   # get specific policy area
+  
+  #get sum of each column for the policy area
+  temp.sum <- total.responses[total.responses$Policy == polarea.temp,]
 
-# merge first two columns into third column
-data$Policy_response <- paste(data$Policy, data$status, sep="_")
+  # take subset of data according to policy area
+  temp.subset <- subset(data, data$Policy == polarea.temp)
+  
+  # normalize subset across columns in subset of data
+  temp.normalize <- as.data.frame(sweep(as.matrix(temp.subset[,3:18]), 2, as.matrix(temp.sum[,2:17]), "/"))
+  
+  # bring back in Policy and status columns
+  temp.normalize$Policy <- temp.subset$Policy
+  temp.normalize$status <- temp.subset$status
+  temp.normalize$Policy_status <- paste(temp.normalize$Policy, temp.normalize$status, sep = "_")
+  
+  # create a new dataset where the normalized data together by rows
+  # if the merged dataset doesn't exist, create it
+  # if the normalized dataset does exist, append to it
+  
+  if (exists("normalized")){
+    normalized<-rbind(normalized, temp.normalize)
+    rm(temp.normalize)
+  }
+  
+  if (!exists("normalized")){
+    normalized <- temp.normalize
+  }
+  
+} # end of loop
+
+# replace Nas with 0's
+normalized[is.na(normalized)] = 0
 
 ###### preprocessing
 # find row names
@@ -86,6 +116,70 @@ d3heatmap(mat.data, scale = "column",
           key = TRUE
           )
 
-# categorize according to the
-# allocate weighting to each categorization
+#################
+# graph according to normalized data
+# preprocessing
 
+# find row names
+rnames <- normalized$Policy_response
+mat.data <- data.matrix(normalized[,1:16])          # convert data to matrix
+rownames(mat.data) <- rnames                  # assign row names 
+
+# creates a 5 x 5 inch image
+png(paste(directory, "WSA_normalized.png", sep = ""),    # create PNG for the heat map        
+    width = 5*300,        # 5 x 300 pixels
+    height = 5*300,
+    res = 300,            # 300 pixels per inch
+    pointsize = 6)        # smaller font size
+
+heatmap.2(mat.data,
+          cellnote = mat.data,  # same data set for cell labels
+          main = "test- WSA paper 
+          (normalized data)", # heat map title
+          notecol="black",      # change font color of cell labels to black
+          density.info="none",  # turns off density plot inside color legend
+          trace="none",         # turns off trace lines inside the heat map
+          margins =c(8,20),     # widens margins around plot
+          col=my_palette,       # use on color palette defined earlier 
+          cexCol=1, 
+          cexRow = 0.6,          # decrease row font size to fit
+          #breaks=col_breaks,    # enable color transition at specified limits
+          dendrogram="row",     # only draw a row dendrogram
+          Colv="NA")            # turn off column clustering
+
+####### second method
+# interactive heatmap
+d3heatmap(mat.data, scale = "column", 
+          dendrogram = "row",
+          col=my_palette,
+          margins =c(8,50), 
+          cexRow=0.6, 
+          cexCol = 0.35,
+          key = TRUE
+)
+
+#####################
+# Categorize according to allocate weighting to each categorization
+# group according to each policy area. Multiply the number of respondents by..
+
+# final score (for stakeholder group for policy area) = 
+# (# respondents for increase/total number of respondents)*3 
+# + (# respondents for maintain/total number of respondents)*2
+# + (# respondents for decrease/total number of respondents)*1
+
+  
+  # 
+  temp.norm <- cbind(temp.subset$Policy, temp.subset$status, temp.norm)
+  
+  # bind to initial data frame
+  
+  
+  #temp.normalize <- sapply(temp.subset[,3:18], 
+  #                         function(x) (x)/temp.sum(x))
+  
+  # status = increase
+  
+  # 
+}
+
+shared <- subset(data1, data1$Policy == 'Shared governance:')
