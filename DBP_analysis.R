e7dcd17a71ecd784991cb29feb876b64bc569847
ashjolly <ashlee.jollymore@gmail.com>
2015-09-29 18:39:35 -0700

    first commit for script that analyzes DBP data for DBP chapter.

---
+++ DBP_analysis.R
@@ -0,0 +1,15 @@
+#
+#
+# Script for analyzing DBP data
+# 29sept2015
+####################
+# Pt 1 - how does chlorination change the spectral composition of EEMS?
+# princip-al component analysis to look at how chlorination altered spectral characteristics (EEMS)
+
+###########
+# Pt 2 - How does EEMS and Water Quality parameters predict DBP formation?
+# thoughts:
+# use machine learning algorythm to predict DBP parameters
+# try multivraite linear model where
+#[DBP] = theta0 + theta1x1+theta2x2
+# where x = spectral and EEM characteristics, as well as water quality parameters (pH, anion concentrations, DOC)
