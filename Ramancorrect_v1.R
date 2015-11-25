06880e3687d3c1881bc726e26a67889a7f279bde
ashjolly <ashlee.jollymore@gmail.com>
2015-11-25 12:13:10 -0800

    Changed to trapz function to calculate error - more accurate when compared to eemR approach

--- Ramancorrect_v1.R
+++ Ramancorrect_v1.R
@@ -11,19 +11,25 @@ Ramancor <- function(blank) {
   y = Raman
   x = em[em375:em430]
   n = length(x)-1
-  summation = 0 
-  iteration = 1
   
-  for (i in 1:n) { 
-    y0 = y[i] 
-    y1 = y[i + 1] 
-    dx = x[i+1] - x[i]
-    summation = summation + dx * (y0 + y1)/2
-    iteration = iteration+1
-  }
+  # first method - slightly underestimates the raman area
+  #summation = 0 
+  #iteration = 1
   
-  BaseRect = (y[1]+y[n])/2*(x[n]-x[1])
-  RamanArea = summation - BaseRect
+  #for (i in 1:n) { 
+  #  y0 = y[i] 
+  #  y1 = y[i + 1] 
+  #  dx = x[i+1] - x[i]
+  #  summation = summation + dx * (y0 + y1)/2
+  #  iteration = iteration+1
+  #}
+  
+  #BaseRect = (y[1]+y[n])/2*(x[n]-x[1])
+  #RamanArea = summation - BaseRect
+  
+  ### Second method - more accurate when compared to eemR results 
+  require(pracma)
+  RamanArea = trapz(x,y)
   
   return(RamanArea)
 } 
