4bcc6ff85e04d1a74a416707ece44bdabc49d957
ashjolly <ashlee.jollymore@gmail.com>
2016-07-01 16:21:56 -0700

    Small changes

--- CRClimateCompilation_function.R
+++ CRClimateCompilation_function.R
@@ -8,12 +8,13 @@ climate.comp <- function(climate.dir) {
   setwd(climate.dir)
   filelist_climate <- list.files(path = climate.dir, pattern = ".csv$")
   climatenames = c("date","Tair","RH","Solar","SoilT","wind", "Precip")
-  climate <- ldply(filelist_climate, read.csv, header=FALSE, skip=1, col.names=climatenames, na.strings = "NaN")
+  climate <- ldply(filelist_climate, read.csv, header=FALSE, skip=1, col.names=climatenames, na.strings = "NaN", 
+                   stringsAsFactors = FALSE)
 
   # change date and time
-  climate$date <- as.POSIXct(strptime(climate$date, format = "%Y-%m-%d %H:%M", tz = "America/Los_Angeles"))
+  climate$date1 <- as.POSIXct(strptime(climate$date, format = "%Y-%m-%d %H:%M", tz = "America/Los_Angeles"))
   #sort according to date
-  climate<- climate[order((climate$date)),] 
+  climate<- climate[order((climate$date1)),] 
   climate <- unique(climate) # get rid of duplicates
   return(climate)
 }
